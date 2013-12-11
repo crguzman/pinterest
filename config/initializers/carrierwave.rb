dd79861e86c2b9b06c43d05c630a444358d1603a
Cristobal Guzman <cristobal.875@gmail.com>
2013-12-11 01:50:24 -0300

    error while uploading in amazon s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,11 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAJKJMOPSLGJHZSYUQ',                        # required
     :aws_secret_access_key  => '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',                        # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :region                 => 'eu-west-2',                  # optional, defaults to 'us-east-1'
+    #:host                   => 's3.example.com',             # optional, defaults to nil
+    #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'pintereston'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
